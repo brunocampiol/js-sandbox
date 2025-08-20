dae45060e5517698331fe0e53338c0f46fd10447
bruno <bruno.campiole@gmail.com>
2025-08-20 13:22:56 -0300

    initial commit

---
+++ src/examples/strings/strings.js
@@ -0,0 +1,7 @@
+// Template literal for string concatenation and interpolation
+let part1 = "Javascript Dynamically";
+let part2 = "Generated Content!";
+let message = `${part1}, ${part2}`;  // Uses backticks to embed variables directly in the string
+
+// Select the <p> element by its ID and set its text content
+document.getElementById('targetMessage').innerText = message;
